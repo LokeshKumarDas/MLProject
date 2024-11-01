af5613575567530e81ab27145b1a13b7b50ab47a
lOKESH <dnishi213@gmail.com>
2024-11-01 13:47:25 +0530

    installed all the required packages

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MLProject",
+    version="0.0.0",
+    author="Lokesh",
+    author_email="lokeshkumardas2004@gmail.com",
+    packages=find_packages()
+)
